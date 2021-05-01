55da1f6c3f05abd2ebb2c11596a2439faad159ef
yrtravi <yrt2492@gmail.com>
2021-05-01 23:56:19 +0530

    'chat'

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+const PORT = 5500 || process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
